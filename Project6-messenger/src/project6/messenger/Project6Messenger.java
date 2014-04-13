816c22adbb1ba31b01c43e8f17a05ef6061a5b83
flarpflinger <tru3gam3r@gmail.com>
2014-04-12 22:12:15 -0700

    project started

---
+++ Project6-messenger/src/project6/messenger/Project6Messenger.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project6.messenger;
+
+/**
+ *
+ * @author dhollis
+ */
+public class Project6Messenger {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
