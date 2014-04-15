9d6b37909e6a6f9db9b2fc87893557449d00e689
flarpflinger <tru3gam3r@gmail.com>
2014-04-14 22:57:59 -0700

    server impl

--- Project6-messenger/src/project6/messenger/ThreadedIMServer.java
+++ Project6-messenger/src/project6/messenger/ThreadedIMServer.java
@@ -1,10 +1,15 @@
 package project6.messenger;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -12,9 +17,12 @@ import java.util.logging.Logger;
 public class ThreadedIMServer
     extends BasicServer implements Runnable {
 
+    private ArrayList<Socket> activeUsers;
+    private ArrayList<String> usernames;
 
   public ThreadedIMServer() {
     super(4225, 0);
+    activeUsers = new ArrayList();
   }
 
   public static void main(String[] args) {
@@ -39,27 +47,73 @@ public class ThreadedIMServer
           String password = null;
           BufferedReader in = new BufferedReader(new InputStreamReader(thisSocket.getInputStream()));
           PrintWriter out = new PrintWriter(thisSocket.getOutputStream(), true);
-          out.println("1 flarpflinger password"); // "1 " + user + password);
+          
           String input = null;
+          boolean notLoggedIn = true;
+          while(notLoggedIn){
+            input = in.readLine();
+            System.out.println(input);
+            if(input.substring(0,1).equals("1")){
+
+                try (BufferedReader br = new BufferedReader(new FileReader("users.txt")))
+                {
+                    String sCurrentLine;
+
+                    int indexUsername = input.indexOf(" ", 2);
+                    System.out.println(indexUsername + ".");
+                    String inputUsername = input.substring(2, indexUsername);
+                    String inputPassword = input.substring(indexUsername+ 1);
+                    while ((sCurrentLine = br.readLine()) != null) {
+                        int index = sCurrentLine.indexOf(" ");
+                        String fileUsername = sCurrentLine.substring(0, index);
+                        String filePassword = sCurrentLine.substring(index);
+                        if(fileUsername.equals(inputUsername) && inputPassword.equals(filePassword)){
+                            out.println("6 " + inputUsername);
+                            notLoggedIn = false;
+                            activeUsers.add(thisSocket);
+                            usernames.add(inputUsername);
+                            break;
+                        }
+                    }
+                    if(notLoggedIn){
+                        out.println("7 " + inputUsername);
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } 
+
+            }
+          }
+          
           while(true){
               input = in.readLine();
               int command = Integer.parseInt(input.substring(0, 1));
-              
+              System.out.println(input);
               switch(command){
+                  case 0:
+                      System.out.println("create login");
+                      createLogin(input);
+                      break;
+                  case 2:
+                      System.out.println("logoff");
+                      String inputUsername = input.substring(2);
+                      broadcast("5 " + inputUsername);
+                      int index = usernames.indexOf(inputUsername);
+                      activeUsers.remove(index);
+                      usernames.remove(index);
+                      break;
                   case 3:
                       System.out.println("message recevied");
-                      break;
-                  case 4:
-                      System.out.println("Buddy on");
-                      break;
-                  case 5:
-                      System.out.println("Buddy off");
-                      break;
-                  case 6:
-                      System.out.println("successful login");
-                      break;
-                  case 7:
-                      System.out.println("Failed login");
+                      int endSender = input.indexOf(' ', 2);
+                      String senderUsername = input.substring(2, endSender);
+                      String recptUsername = input.substring(endSender);
+                      
+                      int indexMessage = usernames.indexOf(recptUsername);
+                      Socket s = activeUsers.remove(indexMessage);
+                      usernames.remove(indexMessage);
+                      //print to socket
+                      //out.println(input);
                       break;
               }
           }
@@ -75,6 +129,22 @@ public class ThreadedIMServer
 	put your readline busy wait
 	**************************/
   }
+
+    private void createLogin(String input) throws IOException {
+        int indexUsername = input.indexOf(" ", 1);
+        System.out.println(indexUsername + ".");
+        String inputUsername = input.substring(1, indexUsername);
+        String inputPassword = input.substring(indexUsername);
+        
+        File file = new File("/users/mkyong/filename.txt");
+        FileWriter fw = new FileWriter(file.getAbsoluteFile(), true);
+	BufferedWriter bw = new BufferedWriter(fw);
+	bw.write(inputUsername + " " + inputPassword);
+    }
+
+    private void broadcast(String string) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
   
 }
 
